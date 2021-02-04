1e2f6fef075c28c7c8140cc124f68660faf2403e
Lazar Marković <lazar.kmarkovic@gmail.com>
2021-02-04 21:24:11 +0100

    Init PG database

--- gitlab_clone/settings.py
+++ gitlab_clone/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gitlab_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'uks',
+        'USER': 'postgres',
+        'PASSWORD': 'devdevdev',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belgrade'
 
 USE_I18N = True
 
