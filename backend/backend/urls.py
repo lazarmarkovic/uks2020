f9091ffc87e087c6f1f01bd5aa69d7d2b0bccf89
Lazar Marković <lazar.kmarkovic@gmail.com>
2021-02-09 22:03:25 +0100

    Use REST, use angular and rest framework

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""gitlab_clone URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r"^accounts/", include("django.contrib.auth.urls")),
-    url(r"^", include("users.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
