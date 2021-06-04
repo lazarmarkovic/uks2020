2c9eceb9403c870c863e3f97d11821b5dc715e9d
Lazar Marković <lazar.kmarkovic@gmail.com>
2021-06-04 23:03:24 +0200

    Add server ip to frontend

--- frontend/src/app/auth/api.interceptor.ts
+++ frontend/src/app/auth/api.interceptor.ts
@@ -7,7 +7,7 @@ export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     if (!req.url.includes("https://raw.githubusercontent.com") && !req.url.includes("https://api.github.com")) {
-      const apiReq = req.clone({ url: `http://localhost:8000${req.url}` });
+      const apiReq = req.clone({ url: `http://65.21.180.192:8080${req.url}` });
       return next.handle(apiReq);
     }
     return next.handle(req);
